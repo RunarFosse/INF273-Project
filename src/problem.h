16b93a49d4c97819da8e227ff9239d0f89297168
Runar Fosse <rfosse01@gmail.com>
2024-01-27 19:49:06 +0100

    Added vehicle struct

--- src/problem.h
+++ src/problem.h
@@ -1,8 +1,17 @@
+#include <vector>
+
+struct {
+    int index;
+    int homeNode;
+    int startTime;
+    int capacity;
+} typedef Vehicle;
 
 class Problem {
     public:
     int noNodes;
     int noVehicles;
     int noCalls;
+    std::vector<Vehicle> vehicles;
     // Todo: Include remaining fields
 };
