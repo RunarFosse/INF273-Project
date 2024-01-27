bf4377f46b87292b43adcf24fea8a7b11cec61ec
Runar Fosse <rfosse01@gmail.com>
2024-01-27 19:50:04 +0100

    Added function docs to Parser::parseProblem

--- src/parser.h
+++ src/parser.h
@@ -1,10 +1,18 @@
 #include <string>
+#include <fstream>
+#include <iostream>
 
 #include "problem.h"
 
 class Parser {
     public:
 
+    /**
+     * @brief Parse a given problem data file into a problem instance.
+     * 
+     * @param path problem data file
+     * @return Problem instance of given data file
+     */
     static Problem parseProblem(std::string path);
 
     private:
