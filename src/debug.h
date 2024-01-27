6d96ea8f10beca7911a5e2ee08ecd37ad255986d
Runar Fosse <rfosse01@gmail.com>
2024-01-27 19:55:08 +0100

    Added debugger class

---
+++ src/debug.h
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "problem.h"
+
+class Debugger {
+    public:
+    /**
+     * @brief Print the problem description to the terminal.
+     * 
+     * @param problem Pointer to problem instance
+     */
+    void printProblem(Problem& problem);
+};
