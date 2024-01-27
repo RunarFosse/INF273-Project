4799d13c80bb8b37726b39e6091ff6bba84a3f7f
Runar Fosse <rfosse01@gmail.com>
2024-01-27 23:18:57 +0100

    Preallocate memory instead of dynamically adding and sorting

--- src/parser.cpp
+++ src/parser.cpp
@@ -19,33 +19,32 @@ Problem Parser::parseProblem(std::string path) {
         std::getline(file, line);
         problem.noVehicles = std::stoi(line);
 
+        problem.vehicles.reserve(problem.noVehicles);
+
         // Parse information per vehicle
         file.ignore(LONG_MAX, '\n');
         for (int i = 0; i < problem.noVehicles; i++) {
             std::getline(file, line);
             int pointer1 = 0, pointer2 = line.find(',');
 
-            Vehicle vehicle;
-            vehicle.index = std::stoi(line.substr(pointer1, pointer2));
+            int index = std::stoi(line.substr(pointer1, pointer2));
+
+            Vehicle* vehicle = &problem.vehicles[index-1];
+            vehicle->index = index;
             pointer1 = pointer2+1, pointer2 = line.find(',', pointer1);
-            vehicle.homeNode = std::stoi(line.substr(pointer1, pointer2));
+            vehicle->homeNode = std::stoi(line.substr(pointer1, pointer2));
             pointer1 = pointer2+1, pointer2 = line.find(',', pointer1);
-            vehicle.startTime = std::stoi(line.substr(pointer1, pointer2));
+            vehicle->startTime = std::stoi(line.substr(pointer1, pointer2));
             pointer1 = pointer2+1;
-            vehicle.capacity = std::stoi(line.substr(pointer1));
-
-            problem.vehicles.push_back(vehicle);
+            vehicle->capacity = std::stoi(line.substr(pointer1));
         }
-        // Sort the vector based on the vehicle index (vehicle index might not match vector index)
-        std::sort(problem.vehicles.begin(), problem.vehicles.end(), [ ](const Vehicle& lhs, const Vehicle& rhs) {
-            return lhs.index < rhs.index;
-        });
-
 
         // Parse the number of calls
         file.ignore(LONG_MAX, '\n');
         std::getline(file, line);
         problem.noCalls = std::stoi(line);
+
+        problem.calls.reserve(problem.noCalls);
 
         // Parse possible calls per vehicle
         file.ignore(LONG_MAX, '\n');
@@ -72,36 +71,32 @@ Problem Parser::parseProblem(std::string path) {
             std::getline(file, line);
             int pointer1 = 0, pointer2 = line.find(',');
 
-            Call call;
-            call.index = std::stoi(line.substr(pointer1, pointer2));
+            int index = std::stoi(line.substr(pointer1, pointer2));
+
+            Call* call = &problem.calls[index-1];
+            call->index = index;
             pointer1 = pointer2+1, pointer2 = line.find(',', pointer1);
-            call.originNode = std::stoi(line.substr(pointer1, pointer2));
+            call->originNode = std::stoi(line.substr(pointer1, pointer2));
             pointer1 = pointer2+1, pointer2 = line.find(',', pointer1);
-            call.destinationNode= std::stoi(line.substr(pointer1, pointer2));
+            call->destinationNode= std::stoi(line.substr(pointer1, pointer2));
             pointer1 = pointer2+1, pointer2 = line.find(',', pointer1);
-            call.size= std::stoi(line.substr(pointer1, pointer2));
+            call->size= std::stoi(line.substr(pointer1, pointer2));
             pointer1 = pointer2+1, pointer2 = line.find(',', pointer1);
-            call.costOfNotTransporting = std::stoi(line.substr(pointer1));
+            call->costOfNotTransporting = std::stoi(line.substr(pointer1));
             
             int lowerbound, upperbound;
             pointer1 = pointer2+1, pointer2 = line.find(',', pointer1);
             lowerbound = std::stoi(line.substr(pointer1, pointer2));
             pointer1 = pointer2+1, pointer2 = line.find(',', pointer1);
             upperbound = std::stoi(line.substr(pointer1, pointer2));
-            call.pickupWindow = std::make_pair(lowerbound, upperbound);
+            call->pickupWindow = std::make_pair(lowerbound, upperbound);
 
             pointer1 = pointer2+1, pointer2 = line.find(',', pointer1);
             lowerbound = std::stoi(line.substr(pointer1, pointer2));
             pointer1 = pointer2+1;
             upperbound = std::stoi(line.substr(pointer1));
-            call.deliveryWindow = std::make_pair(lowerbound, upperbound);
-
-            problem.calls.push_back(call);
+            call->deliveryWindow = std::make_pair(lowerbound, upperbound);
         }
-        // Sort the vector based on the call index (call index might not match vector index)
-        std::sort(problem.calls.begin(), problem.calls.end(), [ ](const Call& lhs, const Call& rhs) {
-            return lhs.index < rhs.index;
-        });
 
         // Close the file after finished reading
         file.close();
