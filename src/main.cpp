e4dc6dd48c112fd30691edc5843aacdcc4957f74
Runar Fosse <rfosse01@gmail.com>
2024-01-27 19:48:42 +0100

    Fixed typo in path

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char const *argv[])
 {
-    Problem problem = Parser::parseProblem("/data/Call_7_Vehicle_3.txt");
+    Problem problem = Parser::parseProblem("data/Call_7_Vehicle_3.txt");
 
     std::cout << "Problem solved" << std::endl;
 
