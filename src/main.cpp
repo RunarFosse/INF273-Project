1c9c37f03a0ab81d185029a5c53523452edb7bd3
Runar Fosse <rfosse01@gmail.com>
2024-01-31 17:54:13 +0100

    Redirected standard output to 'results.txt' file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,3 @@
-#include <string>
-#include <iostream>
-
 #include "testcase.h"
 #include "solution.h"
 #include "parser.h"
@@ -11,13 +8,17 @@ int main(int argc, char const *argv[])
     // Initialize a random number generator
     std::default_random_engine rng = std::default_random_engine{std::random_device {}()};
 
+    // Redirect standard output to 'results.txt'
+    std::ofstream out("results.txt");
+    std::cout.rdbuf(out.rdbuf());
+
     // Run each test case given
     InstanceRunner::blindRandomSearch("Call_7_Vehicle_3", 10, 10000, rng);
     InstanceRunner::blindRandomSearch("Call_18_Vehicle_5", 10, 10000, rng);
-    InstanceRunner::blindRandomSearch("Call_35_Vehicle_7", 10, 10000, rng);
-    InstanceRunner::blindRandomSearch("Call_80_Vehicle_20", 10, 10000, rng);
-    InstanceRunner::blindRandomSearch("Call_130_Vehicle_40", 10, 10000, rng);
-    InstanceRunner::blindRandomSearch("Call_300_Vehicle_90", 10, 10000, rng);
+    //InstanceRunner::blindRandomSearch("Call_35_Vehicle_7", 10, 10000, rng);
+    //InstanceRunner::blindRandomSearch("Call_80_Vehicle_20", 10, 10000, rng);
+    //InstanceRunner::blindRandomSearch("Call_130_Vehicle_40", 10, 10000, rng);
+    //InstanceRunner::blindRandomSearch("Call_300_Vehicle_90", 10, 10000, rng);
 
     return 0;
 }
