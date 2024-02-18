d1b88759a48990d63dc5dbea8965b6862dd97e88
Runar Fosse <rfosse01@gmail.com>
2024-02-18 23:45:35 +0100

    Run Local Search and Simulated Annealing

--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,11 @@ int main(int argc, char const *argv[])
     std::default_random_engine rng = std::default_random_engine{std::random_device {}()};
 
     // Run each test case given
+    Debugger::outputToFile("results_ls.txt");
+    InstanceRunner::testAlgorithm(InstanceRunner::localSearch, false, 10, 10000, rng);
+
     Debugger::outputToFile("results_sa.txt");
     InstanceRunner::testAlgorithm(InstanceRunner::simulatedAnnealing, false, 10, 10000, rng);
-    
-    /* // Verify correct implementation of 1-insert
-    Problem problem = Parser::parseProblem("data/Call_7_Vehicle_3.txt");
-    Solution initial = Solution::initialSolution(&problem);
-    Solution neighbour = initial.getNeighbour(rng);
-
-    Debugger::printSolution(&initial);
-    Debugger::printSolution(&neighbour);
-    */
 
     return 0;
 }
